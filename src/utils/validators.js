a0ba566445fced20a9684bfa1a48eac0a574a6c2
YusufKarahan20 <ysf.krhn.20@gmail.com>
2025-07-16 18:14:17 +0300

    Project cleanup and folder restructuring

--- src/utils/validators.js
+++
@@ -1,9 +0,0 @@
-export const isValidEmail = (email) =>
-  /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-
-export const passwordStrength = (pwd) => {
-  if (pwd.length < 6) return "Şifre en az 6 karakter olmalı.";
-  if (!/[A-Z]/.test(pwd) || !/[0-9]/.test(pwd))
-    return "Şifre büyük harf ve sayı içermeli.";
-  return "";
-};
